becd676c38f8f79ade6cb33e5c61abe22ef0e05b
Anthony Amiluddin <anthonyamiluddin1@gmail.com>
2025-10-23 08:49:20 +0800

    installed shadcn with lucide react (#1)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/jsx', function () {
-    return Inertia::render('jsx');
+Route::get('/', function () {
+    return Inertia::render('hr/dashboard');
 });
